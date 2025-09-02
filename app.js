7e56573c9b97d42ed3409d0aecfedd86439ad19d
katya-test <katyasuhr@gmail.com>
2025-09-02 12:43:21 -0400

    week10

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
 const express = require('express');
 const app = express();
 
@@ -16,7 +24,15 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
+
 // extra packages
+app.use(rateLimiter({
+  windowMs: 15*60*1000, //15 minutes
+  max: 100, //limit each IP to 100 request per window
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // implements routes
 app.use('/api/v1/auth', authRouter)
